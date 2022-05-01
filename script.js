04d898d231e46899854cb0f2c924816aef70b923
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-04-30 22:11:44 -0300

    requisito 2

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const textInput = document.querySelector('#text-input');
 const memeText = document.querySelector('#meme-text');
 
+const imageInput = document.querySelector('#meme-insert');
+const memeImage = document.querySelector('#meme-image');
+
 function handleTextInputToMeme() {
   textInput.addEventListener('input', () => {
     memeText.innerHTML = textInput.value;
@@ -8,3 +11,18 @@ function handleTextInputToMeme() {
 }
 
 handleTextInputToMeme();
+
+imageInput.addEventListener('change', (e) => {
+  const reader = new FileReader();
+  const image = imageInput.files[0];
+
+  console.log(image);
+
+  reader.addEventListener('load', () => {
+    console.log(reader)
+    console.log(reader.result)
+    memeImage.setAttribute('src', reader.result);
+  });
+
+  reader.readAsDataURL(image);
+});
